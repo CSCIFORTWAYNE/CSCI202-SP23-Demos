209a3c1cabd08c2e0e0d69f35650a2a082e6c6a9
Angela <avenable11@ivytech.edu>
2023-02-09 22:25:11 -0500

    unit test 2/9/2023

---
+++ 02_09_23/unit test/tests.cpp
@@ -0,0 +1,28 @@
+#include "arrayStack.h"
+#include "linkedStack.h"
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <stdexcept>
+
+TEST(stackTest, arrayStackTest)
+{
+    arrayStack<int> as;
+    EXPECT_THROW(as.pop(), std::underflow_error);
+    EXPECT_THROW(as.top(), std::out_of_range);
+    EXPECT_TRUE(as.isEmptyStack());
+    for (int i = 0; i < 100; i++)
+    {
+        as.push(i);
+    }
+    EXPECT_TRUE(as.isFullStack());
+    EXPECT_THROW(as.push(100), std::overflow_error);
+    arrayStack<int> as2(20);
+    arrayStack<int> as3(as);
+}
+
+int main(int x, char **y)
+{
+    testing::InitGoogleTest(&x, y);
+    int code = RUN_ALL_TESTS();
+    return code;
+}
